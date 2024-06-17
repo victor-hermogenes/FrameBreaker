a0788193bedaf3c5033c3d5ceeaeee71ce61bf57
victor-hermogenes <victorltrgh@gmail.com>
2024-06-17 15:23:18 -0300

    Pause/return adjusted and working

--- video_functions.py
+++ video_functions.py
@@ -12,35 +12,29 @@ def start_video(video_file, player, speed, playing, label):
 
 
 def play_video(player, playing, label):
-    while playing[0]:
-        frame, val = player.get_frame()
-        if val == 'eof':
-            playing[0] = False
-            break
-        if frame is None:
-            continue
-        img, t = frame
-        img_bytes = img.to_bytearray()[0]
-        img = np.frombuffer(img_bytes, np.uint8).reshape(img.get_size()[1], img.get_size()[0], 3)
-        img = Image.fromarray(img)
-        img = ImageTk.PhotoImage(img)
-        label.config(image=img)
-        label.image = img
-        if player.get_pause():
-            while player.get_pause():
-                if not playing[0]:
-                    break
+    while True:
+        if playing[0]:
+            frame, val = player.get_frame()
+            if val == 'eof':
+                playing[0] = False
+                break
+            if frame is None:
                 continue
+            img, t = frame
+            img_bytes = img.to_bytearray()[0]
+            img = np.frombuffer(img_bytes, np.uint8).reshape(img.get_size()[1], img.get_size()[0], 3)
+            img = Image.fromarray(img)
+            img = ImageTk.PhotoImage(img)
+            label.config(image=img)
+            label.image = img
+        else:
+            player.set_pause(True)
 
 
 def pause_video(player, playing):
     if player:
-        if player.get_pause():
-            player.set_pause(False)
-            playing[0] = True
-        else:
-            player.set_pause(True)
-            playing[0] = False
+        playing[0] = not playing[0]
+        player.set_pause(not playing[0])
 
 
 def advance_video(player):
