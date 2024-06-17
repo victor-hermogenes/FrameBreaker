b61f0b08cda3b0a13a86725f8c638663cd60dd23
victor-hermogenes <victorltrgh@gmail.com>
2024-06-17 13:42:58 -0300

    Making improvements to display video

--- video_functions.py
+++ video_functions.py
@@ -1,15 +1,17 @@
 from ffpyplayer.player import MediaPlayer
 import threading
+from PIL import Image, ImageTk
+import tkinter as tk
 
 
-def start_video(video_file, player, speed, playing):
-    player= MediaPlayer(video_file, ff_opts={'paused': False})
-    playing[0]
-    threading.Thread(target=play_video, args=(player, speed, playing)).start()
+def start_video(video_file, player, speed, playing, label):
+    player = MediaPlayer(video_file, ff_opts={'paused': False, 'af': f'atempo={speed}'})
+    playing[0] = True
+    threading.Thread(target=play_video, args=(player, playing, label)).start()
     return player
 
 
-def play_video(player, speed, playing):
+def play_video(player, playing, label):
     while playing[0]:
         frame, val = player.get_frame()
         if val == 'eof':
@@ -18,7 +20,10 @@ def play_video(player, speed, playing):
             continue
         img, t = frame
         img = img.to_image()
-        img.show()
+        img = Image.frombytes('RGB', img.get_size(), img.to_bytearray()[0])
+        img = ImageTk.PhotoImage(img)
+        label.config(image=img)
+        label.image = img
 
 
 def pause_video(player, playing):
@@ -35,8 +40,3 @@ def advance_video(player):
 def goback_video(player):
     if player:
         player.seek(-10, relative=True)
-
-    
-def set_speed(player, speed):
-    if player:
-        player.set_speed(speed)
