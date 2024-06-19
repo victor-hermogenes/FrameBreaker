037d025c04e82efa9e5bc735ed109165a539159b
victor-hermogenes <victorltrgh@gmail.com>
2024-06-19 10:57:22 -0300

    Trying to create fullscreen functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QFileDialog
 from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import QTimer, Qt
 import sys
 import video_functions as vf
 
@@ -13,7 +14,8 @@ class VideoPlayer(QMainWindow):
         self.setGeometry(100, 100, 800, 600)
 
 
-        videoWidget = QVideoWidget()
+        self.videoWidget = QVideoWidget()
+
 
         self.openButton = QPushButton("Open Video")
         self.openButton.clicked.connect(self.open_file)
@@ -23,10 +25,23 @@ class VideoPlayer(QMainWindow):
         self.startPauseButton.setEnabled(False)
 
 
+        self.fullscreenButton = QPushButton("Full Screen")
+        self.fullscreenButton.clicked.connect(self.toggle_fullscreen)
+
+
         layout = QVBoxLayout()
-        layout.addWidget(videoWidget)
+        layout.addWidget(self.videoWidget)
         layout.addWidget(self.openButton)
         layout.addWidget(self.startPauseButton)
+        layout.addWidget(self.fullscreenButton)
+
+        self.controls = QWidget(self)
+        self.controls.setLayout(layout)
+
+
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.videoWidget)
+        mainLayout.addWidget(self.controls)
 
 
         widget = QWidget(self)
@@ -34,8 +49,19 @@ class VideoPlayer(QMainWindow):
         self.setCentralWidget(widget)
 
 
-        self.mediaPlayer = vf.create_media_player(videoWidget)
+        self.mediaPlayer = vf.create_media_player(self.videoWidget)
         self.is_paused = True
+        self.is_fullscreen = False
+
+
+        self.mouse_timer = QTimer(self)
+        self.mouse_timer.setInterval(1000)
+        self.mouse_timer.timeout.connect(self.hide_controls)
+
+
+        self.setMouseTracking(True)
+        self.videoWidget.setMouseTracking(True)
+        self.controls.setMouseTracking(True)
 
 
     def open_file(self):
@@ -50,6 +76,35 @@ class VideoPlayer(QMainWindow):
     def start_pause_video(self):
         self.is_paused = vf.toggle_play_pause(self.mediaPlayer, self.is_paused)
         self.startPauseButton.setText("Pause" if not self.is_paused else "Start")
+
+
+    def toggle_fullscreen(self):
+        if self.is_fullscreen:
+            self.showNormal()
+            self.setWindowFlags(self.windowFlags() & ~Qt.FramelessWindowHint)
+            self.show()
+            self.fullscreenButton.setText("Full Screen")
+        else:
+            self.showFullScreen()
+            self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
+            self.show()
+            self.fullscreenButton.setText("Exit Full Screen")
+            self.mouse_timer.start()
+
+
+        self.is_fullscreen = not self.is_fullscreen
+
+
+    def hide_controls(self):
+        if self.is_fullscreen:
+            self.controls.hide()
+
+
+    def mouseMoveEvent(self, event):
+        if self.is_fullscreen:
+            self.controls.show()
+            self.mouse_timer.start()
+        super().mouseMoveEvent(event)
 
 
 if __name__ == "__main__":
